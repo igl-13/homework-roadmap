4bea2617291eb20f8d1844212e45040f583a8ba8
igl-13 <jigolnitsyn@gmail.com>
2025-05-28 09:16:14 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Typography variant="h3" component="h1" gutterBottom>
+        Системный лидген - Домашние задания
+      </Typography>
+    </Container>
+  );
+};
+
+export default App; 
